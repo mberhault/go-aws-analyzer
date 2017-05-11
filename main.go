a018227cdec3917b01b46f24c99f2d5fcb989482
marc <marc@cockroachlabs.com>
2017-05-11 12:32:40 -0400

    Reverse list before writing template

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -52,6 +53,11 @@ func main() {
 		log.Fatalf("could not create summary files: %v", err)
 	}
 	defer f.Close()
-	writeHTML(f, "Daily downloads", daily[len(daily)-10:])
-	writeHTML(f, "Weekly downloads", weekly[len(weekly)-10:])
+
+	// We need to reverser the data first, templates don't have for loops.
+	sort.Sort(sort.Reverse(daily))
+	sort.Sort(sort.Reverse(weekly))
+
+	writeHTML(f, "Daily downloads", daily[0:10])
+	writeHTML(f, "Weekly downloads", weekly[0:10])
 }
